3533b56b3cbe5e38aff9d98a4274dc5b218a4564
Владислав Владимирович <vladislavvladimirovic@MacBook-Air-Vladislav.local>
2024-05-14 15:09:57 +0300

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+use NOT EUROPIAN COUNTRY in VPN!
+My recommendation: "Veepn"
+
+что нужно доработать:
+- add localstorage
+- all buttons and icons should be usability
+- registration block (+/-)
+- suggestion questions should be visible in loading process
---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+import history from './history.png'
+import menu from './menu.png'
+import plus from './plus.png'
+import question from './question.png'
+import setting from './setting.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.png'
+import code from './code.png'
+import send_icon from './send_icon.png'
+import gemini_icon from './gemini_icon.png'
+import hero_icon from './hero_icon.png'
+import compass from './compass.png'
+import comments from './comments.png'
+import bulb from './lamp.png'
+
+export const assets = {
+    history,
+    menu,
+    plus,
+    question,
+    setting,
+    bulb,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    youtube_icon,
+    message_icon,
+    code,
+    send_icon,
+    gemini_icon,
+    hero_icon,
+    compass,
+    comments
+}
